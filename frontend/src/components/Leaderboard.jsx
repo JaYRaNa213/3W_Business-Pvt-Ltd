3e53b2253403cf8b557a84d70039304303d52afc
JaYRaNa213 <tresurejay6@gmail.com>
2025-07-18 14:53:16 +0530

    color2

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,68 +1,16 @@
-// import React from "react";
-// import TopThreeCards from "./TopThreeCards";
-// import RemainingList from "./RemainingList";
-// import ClaimButton from "./ClaimButton";
-// import { Box, Button, Container } from "@mui/material";
-// import { useNavigate } from "react-router-dom";
-
-// const Leaderboard = ({
-//   data,
-//   currentUserId,
-  
-//   onClaim,
-// }) => {
-//   const navigate = useNavigate();
-
-//   const sortedData = [...data].sort((a, b) => b.totalPoints - a.totalPoints);
-//   const topThree = [sortedData[1] || {}, sortedData[0] || {}, sortedData[2] || {}];
-//   const remaining = sortedData.slice(3);
-
-//   return (
-//     <Container maxWidth="sm" sx={{ px: { xs: 2, sm: 4 }, py: { xs: 2, sm: 4 } }}>
-//       {currentUserId && (
-//         <Box display="flex" justifyContent="center" mb={3}>
-//           <ClaimButton userId={currentUserId} onClaim={onClaim} />
-//         </Box>
-//       )}
-
-//       <Box
-//         display="flex"
-//         flexDirection={{ xs: "column", sm: "row" }}
-//         alignItems="center"
-//         justifyContent="center"
-//         gap={2}
-//         mb={4}
-//       >
-//         <TopThreeCards users={topThree} currentUserId={currentUserId} />
-//       </Box>
-
-//       <Box display="flex" justifyContent="center">
-//         <Box width="100%" maxWidth={500}>
-//           <RemainingList users={remaining} currentUserId={currentUserId} />
-//         </Box>
-//       </Box>
-
-//       {currentUserId && (
-//         <Box display="flex" justifyContent="flex-end" mt={4}>
-          
-//         </Box>
-//       )}
-//     </Container>
-//   );
-// };
-
-// export default Leaderboard;
-
-
-
 import React from "react";
 import TopThreeCards from "./TopThreeCards";
 import RemainingList from "./RemainingList";
 import ClaimButton from "./ClaimButton";
-import { Box, Container } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-const Leaderboard = ({ data, currentUserId, onClaim }) => {
+const Leaderboard = ({
+  data,
+  currentUserId,
+  
+  onClaim,
+}) => {
   const navigate = useNavigate();
 
   const sortedData = [...data].sort((a, b) => b.totalPoints - a.totalPoints);
@@ -70,15 +18,7 @@ const Leaderboard = ({ data, currentUserId, onClaim }) => {
   const remaining = sortedData.slice(3);
 
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        backgroundColor: "#bdbb28ff", // 🎨 School Bus Yellow
-        borderRadius: 2,
-        p: { xs: 2, sm: 4 },
-        boxShadow: 4,
-      }}
-    >
+    <Container maxWidth="sm" sx={{ px: { xs: 2, sm: 4 }, py: { xs: 2, sm: 4 } }}>
       {currentUserId && (
         <Box display="flex" justifyContent="center" mb={3}>
           <ClaimButton userId={currentUserId} onClaim={onClaim} />
@@ -104,7 +44,7 @@ const Leaderboard = ({ data, currentUserId, onClaim }) => {
 
       {currentUserId && (
         <Box display="flex" justifyContent="flex-end" mt={4}>
-          {/* Reserved space for future buttons */}
+          
         </Box>
       )}
     </Container>
@@ -112,3 +52,63 @@ const Leaderboard = ({ data, currentUserId, onClaim }) => {
 };
 
 export default Leaderboard;
+
+
+
+// import React from "react";
+// import TopThreeCards from "./TopThreeCards";
+// import RemainingList from "./RemainingList";
+// import ClaimButton from "./ClaimButton";
+// import { Box, Container } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+
+// const Leaderboard = ({ data, currentUserId, onClaim }) => {
+//   const navigate = useNavigate();
+
+//   const sortedData = [...data].sort((a, b) => b.totalPoints - a.totalPoints);
+//   const topThree = [sortedData[1] || {}, sortedData[0] || {}, sortedData[2] || {}];
+//   const remaining = sortedData.slice(3);
+
+//   return (
+//     <Container
+//       maxWidth="sm"
+//       sx={{
+//         backgroundColor: "#bdbb28ff", // 🎨 School Bus Yellow
+//         borderRadius: 2,
+//         p: { xs: 2, sm: 4 },
+//         boxShadow: 4,
+//       }}
+//     >
+//       {currentUserId && (
+//         <Box display="flex" justifyContent="center" mb={3}>
+//           <ClaimButton userId={currentUserId} onClaim={onClaim} />
+//         </Box>
+//       )}
+
+//       <Box
+//         display="flex"
+//         flexDirection={{ xs: "column", sm: "row" }}
+//         alignItems="center"
+//         justifyContent="center"
+//         gap={2}
+//         mb={4}
+//       >
+//         <TopThreeCards users={topThree} currentUserId={currentUserId} />
+//       </Box>
+
+//       <Box display="flex" justifyContent="center">
+//         <Box width="100%" maxWidth={500}>
+//           <RemainingList users={remaining} currentUserId={currentUserId} />
+//         </Box>
+//       </Box>
+
+//       {currentUserId && (
+//         <Box display="flex" justifyContent="flex-end" mt={4}>
+//           {/* Reserved space for future buttons */}
+//         </Box>
+//       )}
+//     </Container>
+//   );
+// };
+
+// export default Leaderboard;
